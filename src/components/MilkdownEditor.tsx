ba0f40f39f823a4ac5b4d5f1ff0d5389d6bd0b5c
so1ve <nn_201312@163.com>
2022-06-30 17:29:29 +0800

    chore(deps): use @dolan-x/milkdown-plugins

--- src/components/MilkdownEditor.tsx
+++ src/components/MilkdownEditor.tsx
@@ -1,38 +1,10 @@
 import type { FC } from "react";
-import type { MilkdownPlugin } from "@milkdown/core";
 import { Editor, defaultValueCtx, rootCtx } from "@milkdown/core";
 import { ReactEditor, useEditor } from "@milkdown/react";
-import { nord } from "@milkdown/theme-nord";
-import { gfm } from "@milkdown/preset-gfm";
-import { emoji } from "@milkdown/plugin-emoji";
-import { indent } from "@milkdown/plugin-indent";
-import { math } from "@milkdown/plugin-math";
-import { slash } from "@milkdown/plugin-slash";
-import { tooltip } from "@milkdown/plugin-tooltip";
-import { upload } from "@milkdown/plugin-upload";
-import { menu } from "@milkdown/plugin-menu";
-import { history } from "@milkdown/plugin-history";
-import { clipboard } from "@milkdown/plugin-clipboard";
-import { prism } from "@milkdown/plugin-prism";
 import { listener, listenerCtx } from "@milkdown/plugin-listener";
+import { milkdownPlugins } from "@dolan-x/milkdown-plugins";
 
 import "prism-themes/themes/prism-nord.css";
-
-const plugins: (MilkdownPlugin | MilkdownPlugin[])[] = [
-  nord,
-  gfm,
-  emoji,
-  indent,
-  math,
-  slash,
-  tooltip,
-  upload,
-  menu,
-  history,
-  clipboard,
-  prism,
-  listener,
-];
 
 interface MilkdownEditorProps {
   value?: string
@@ -50,7 +22,8 @@ const MilkdownEditor: FC<MilkdownEditorProps> = ({ value, onChange }) => {
           });
         }
       })
-      .use(plugins.flat()),
+      .use(listener)
+      .use(milkdownPlugins),
   );
 
   return <ReactEditor editor={editor} />;
