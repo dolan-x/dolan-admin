7ee6df4d0315adfdffda39962689d1d3949bce12
so1ve <nn_201312@163.com>
2023-01-09 17:41:39 +0800

    feat: move base urls to site config

--- src/pages/dash/config/functions.tsx
+++ src/pages/dash/config/functions.tsx
@@ -1,20 +1,21 @@
 import type { FC } from "react";
-import { Button, Toast } from "@douyinfe/semi-ui";
+// import { Button, Toast } from "@douyinfe/semi-ui";
 import type { ConfigFunctions } from "@dolan-x/shared";
 import useAsyncEffect from "use-async-effect";
 
-import { FormWrapper, Loading, SemiInput } from "~/components/Dash/Common";
+// import { SemiInput } from "~/components/Dash/Common";
+import { FormWrapper, Loading } from "~/components/Dash/Common";
 import { fetchApi } from "~/lib";
 
 const Functions: FC = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   const [loading, setLoading] = useState(true);
-  const [saving, setSaving] = useState(false);
-  const [sitemapPostsBaseUrl, setSitemapPostsBaseUrl] = useState("");
-  const [sitemapCategoriesBaseUrl, setSitemapCategoriesBaseUrl] = useState("");
-  const [sitemapTagsBaseUrl, setSitemapTagsBaseUrl] = useState("");
-  const [sitemapPagesBaseUrl, setSitemapPagesBaseUrl] = useState("");
+  // const [saving, setSaving] = useState(false);
+  // const [sitemapPostsBaseUrl, setSitemapPostsBaseUrl] = useState("");
+  // const [sitemapCategoriesBaseUrl, setSitemapCategoriesBaseUrl] = useState("");
+  // const [sitemapTagsBaseUrl, setSitemapTagsBaseUrl] = useState("");
+  // const [sitemapPagesBaseUrl, setSitemapPagesBaseUrl] = useState("");
 
   async function onFetch() {
     let resp;
@@ -25,18 +26,18 @@ const Functions: FC = () => {
       return;
     }
     if (resp.success) {
-      const {
-        sitemap: {
-          postsBaseUrl,
-          categoriesBaseUrl,
-          tagsBaseUrl,
-          pagesBaseUrl,
-        },
-      } = resp.data;
-      setSitemapPostsBaseUrl(postsBaseUrl);
-      setSitemapCategoriesBaseUrl(categoriesBaseUrl);
-      setSitemapTagsBaseUrl(tagsBaseUrl);
-      setSitemapPagesBaseUrl(pagesBaseUrl);
+      // const {
+      //   sitemap: {
+      //     postsBaseUrl,
+      //     categoriesBaseUrl,
+      //     tagsBaseUrl,
+      //     pagesBaseUrl,
+      //   },
+      // } = resp.data;
+      // setSitemapPostsBaseUrl(postsBaseUrl);
+      // setSitemapCategoriesBaseUrl(categoriesBaseUrl);
+      // setSitemapTagsBaseUrl(tagsBaseUrl);
+      // setSitemapPagesBaseUrl(pagesBaseUrl);
       setLoading(false);
     } else {
       // Toast.error
@@ -44,38 +45,38 @@ const Functions: FC = () => {
   }
   useAsyncEffect(onFetch, []);
 
-  async function onSave() {
-    setSaving(true);
-    const body: ConfigFunctions = {
-      sitemap: {
-        postsBaseUrl: sitemapPostsBaseUrl,
-        categoriesBaseUrl: sitemapCategoriesBaseUrl,
-        tagsBaseUrl: sitemapTagsBaseUrl,
-        pagesBaseUrl: sitemapPagesBaseUrl,
-      },
-    };
-    try {
-      await fetchApi("config/functions", {
-        method: "PUT",
-        body,
-      });
-      Toast.success(t("common.save-success"));
-    } catch (e: any) {
-      Toast.error(t("common.save-failed") + e?.data?.error);
-    }
-    setSaving(false);
-  }
+  // async function onSave() {
+  //   setSaving(true);
+  //   const body: ConfigFunctions = {
+  //     sitemap: {
+  //       postsBaseUrl: sitemapPostsBaseUrl,
+  //       categoriesBaseUrl: sitemapCategoriesBaseUrl,
+  //       tagsBaseUrl: sitemapTagsBaseUrl,
+  //       pagesBaseUrl: sitemapPagesBaseUrl,
+  //     },
+  //   };
+  //   try {
+  //     await fetchApi("config/functions", {
+  //       method: "PUT",
+  //       body,
+  //     });
+  //     Toast.success(t("common.save-success"));
+  //   } catch (e: any) {
+  //     Toast.error(t("common.save-failed") + e?.data?.error);
+  //   }
+  //   setSaving(false);
+  // }
 
   return (
     <FormWrapper>
       <Loading loading={loading}>
-        <SemiInput label={t("pages.config.functions.sitemap.posts-baseurl")} value={sitemapPostsBaseUrl} onChange={setSitemapPostsBaseUrl} />
+        {/* <SemiInput label={t("pages.config.functions.sitemap.posts-baseurl")} value={sitemapPostsBaseUrl} onChange={setSitemapPostsBaseUrl} />
         <SemiInput label={t("pages.config.functions.sitemap.categories-baseurl")} value={sitemapCategoriesBaseUrl} onChange={setSitemapCategoriesBaseUrl} />
         <SemiInput label={t("pages.config.functions.sitemap.tags-baseurl")} value={sitemapTagsBaseUrl} onChange={setSitemapTagsBaseUrl} />
         <SemiInput label={t("pages.config.functions.sitemap.pages-baseurl")} value={sitemapPagesBaseUrl} onChange={setSitemapPagesBaseUrl} />
         <Button theme="solid" disabled={loading} loading={saving} onClick={onSave}>
           {t("common.save")}
-        </Button>
+        </Button> */}
       </Loading>
     </FormWrapper>
   );
--- src/pages/dash/config/site.tsx
+++ src/pages/dash/config/site.tsx
@@ -15,6 +15,10 @@ const Site: FC = () => {
   const [description, setDescription] = useState("");
   const [keywords, setKeywords] = useState<string[]>([]);
   const [url, setUrl] = useState("");
+  const [postsBaseUrl, setPostsBaseUrl] = useState("");
+  const [pagesBaseUrl, setPagesBaseUrl] = useState("");
+  const [tagsBaseUrl, setTagsBaseUrl] = useState("");
+  const [categoriesBaseUrl, setCategoriesBaseUrl] = useState("");
 
   async function onFetch() {
     let resp;
@@ -30,11 +34,19 @@ const Site: FC = () => {
         description,
         keywords,
         url,
+        postsBaseUrl,
+        pagesBaseUrl,
+        tagsBaseUrl,
+        categoriesBaseUrl,
       } = resp.data;
       setName(name);
       setDescription(description);
       setKeywords(keywords);
       setUrl(url);
+      setPostsBaseUrl(postsBaseUrl);
+      setPagesBaseUrl(pagesBaseUrl);
+      setTagsBaseUrl(tagsBaseUrl);
+      setCategoriesBaseUrl(categoriesBaseUrl);
       setLoading(false);
     } else {
       // Toast.error
@@ -44,11 +56,15 @@ const Site: FC = () => {
 
   async function onSave() {
     setSaving(true);
-    const body = {
+    const body: ConfigSite = {
       name,
       description,
       keywords,
       url,
+      postsBaseUrl,
+      pagesBaseUrl,
+      tagsBaseUrl,
+      categoriesBaseUrl,
     };
     try {
       await fetchApi("config/site", {
@@ -69,6 +85,10 @@ const Site: FC = () => {
         <SemiTextArea label={t("pages.config.site.description")} value={description} onChange={setDescription} />
         <SemiTagInput label={t("pages.config.site.keywords")} value={keywords} onChange={setKeywords} />
         <SemiInput label={t("pages.config.site.url")} value={url} onChange={setUrl} />
+        <SemiInput label={t("pages.config.site.postsBaseUrl")} value={postsBaseUrl} onChange={setPostsBaseUrl} />
+        <SemiInput label={t("pages.config.site.pagesBaseUrl")} value={pagesBaseUrl} onChange={setPagesBaseUrl} />
+        <SemiInput label={t("pages.config.site.tagsBaseUrl")} value={tagsBaseUrl} onChange={setTagsBaseUrl} />
+        <SemiInput label={t("pages.config.site.categoriesBaseUrl")} value={categoriesBaseUrl} onChange={setCategoriesBaseUrl} />
         <Button theme="solid" disabled={loading} loading={saving} onClick={onSave}>
           {t("common.save")}
         </Button>
